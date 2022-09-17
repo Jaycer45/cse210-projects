36aa1dd623830128e5d31adc2887aff39ab1936f
Use your GitHub username <Enter your email address>
2022-09-17 12:51:51 -0700

    g

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,11 @@ namespace Prep1
         {
             Console.WriteLine("This is prep 1");
 
-            // Write your code here
+            Console.WriteLine("What is your first name? ");
+            string first = Console.ReadLine();
+            Console.WriteLine("What is your first name? ");
+            string last = Console.ReadLine();
+            Console.WriteLine($"Your name is {last}, {first} {last}");
         }
     }
 }
